908ddd37a73d7e861fad9855f6a5721f66b7513a
GemmaG7 <gemmagrat921@gmail.com>
2023-11-27 15:30:57 +0000

    employees mapping

--- src/Components/Employees.js
+++ src/Components/Employees.js
@@ -1,15 +1,51 @@
-function doubleIt(x){
-    return x*2
-}
+
 
 function Employees(){
 
-    let data =[2,3,5,6]
+    let data =[2,3,5,6];
+
+    let employees=[
+        {
+            regNo: 145, 
+            name:"Shafeeq", 
+            salary: 2300,
+            department:"IT"
+        }, 
+        {
+            regNo: 136, 
+            name:"Shafeeq2", 
+            salary: 5300,
+            department:"Sales"
+        },
+        {
+            regNo: 102, 
+            name:"Shafeeq4", 
+            salary: 1900,
+            department:"HR"
+        }
+        
+    ]
     
     return(
         <>
-        {data.map(doubleIt)
-}
+        <table>
+            <tr>
+                <th>Employee Number</th>
+                <th>Name</th>
+                <th>Salary</th>
+                <th>Department</th>
+            </tr>
+            {
+                employees.map( (X) =>
+                <tr>
+                    <td>{X.regNo}</td>
+                    <td>{X.name}</td>
+                    <td>{X.salary}</td>
+                    <td>{X.department}</td>
+                </tr>
+                ) 
+            }
+        </table>
         </>
     );
 }
