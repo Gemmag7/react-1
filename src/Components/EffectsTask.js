913dca7610730d8956d000a6d9162e8a9694012f
GemmaG7 <gemmagrat921@gmail.com>
2023-11-29 09:53:57 +0000

    UseEffect task done

--- src/Components/EffectsTask.js
+++ src/Components/EffectsTask.js
@@ -3,11 +3,15 @@ import {useEffect, useState} from "react";
 function EffectsTask(){
 
     
-    
+    //Subjects array that has data such as the trainer name and the subject they teach
     let subjects =[
         {
             trainerName: "Shafeeq", 
             subject: "HTML"
+        }, 
+        {
+            trainerName: "Shafeeq", 
+            subject: "SQL"
         }, 
         {
             trainerName: "Andy", 
@@ -24,14 +28,20 @@ function EffectsTask(){
         {
             trainerName: "Shafeeq", 
             subject: "JavaScript"
+        },
+        {
+            trainerName: "Tadas", 
+            subject: "Angular"
         }
     ]
     let[trainerName, setTrainerName] = useState("")
     let[newSubjectList, setNewSubjectList] = useState(subjects)
 
+    /**This is only executed when a search value is entered and changes the whole subjecct list with filtered data */
     useEffect( () =>  {
         setNewSubjectList (subjects.filter((rec) => rec.trainerName ==trainerName))
     },[trainerName])
+
    function filterTrainers(){
     setTrainerName(document.getElementById("txtName").value)
    
@@ -42,17 +52,19 @@ function EffectsTask(){
         <input id="txtName" type="text" />
         <button id="searchBtn" className="button" onClick={() => filterTrainers()} >Search</button>
         <br/>
-       
+       Trainer: <b>{trainerName}</b>
+        <br/>
+        <br/>
         <br/>
         <table >
             <tr>
-            <th>Subject Number</th>
-            <th>Trainer</th>
+            <th>Subject</th>
+            {/**<th>Trainer</th>*/}
             </tr>
             {newSubjectList.map((subject) =>(
             <tr>
                 <td>{subject.subject}</td>
-                <td>{subject.trainerName}</td>
+               {/**  <td>{subject.trainerName}</td>**/}
             </tr>
             )
       )}
